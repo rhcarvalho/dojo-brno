69bd518f4d95216fafb27365d6bc1ea5aaad1053
Rodolfo Carvalho <rhcarvalho@gmail.com>
2016-07-14 00:43:14 +0200

    Refactor to call fizzbuzz everywhere
    We should be using fizzbuzz to compute the value we want to print in all
    cases.
    
    Using a hack to convert numbers to strings, works for now.

--- 2016-07-13/fizzbuzz/fizzbuzz.go
+++ 2016-07-13/fizzbuzz/fizzbuzz.go
@@ -2,19 +2,22 @@ package fizzbuzz
 
 func Print(print func(string), from, to int) {
 	if n := 1; from <= n && n <= to {
-		print("1")
+		print(fizzbuzz(1))
 	}
 	if n := 2; from <= n && n <= to {
-		print("2")
+		print(fizzbuzz(2))
 	}
 	if n := 3; from <= n && n <= to {
 		print(fizzbuzz(3))
 	}
 	if n := 4; from <= n && n <= to {
-		print("4")
+		print(fizzbuzz(4))
 	}
 }
 
-func fizzbuzz(int) string {
-	return "Fizz"
+func fizzbuzz(n int) string {
+	if n == 3 {
+		return "Fizz"
+	}
+	return string('0' + n)
 }
